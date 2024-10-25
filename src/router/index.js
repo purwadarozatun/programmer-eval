86d75343def6c2b0b3dfadb532cedfd7ecb2e22f
Purwa <purwadarozatunakis@gmail.com>
2024-10-25 11:19:05 +0700

    change path

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/programmer-eval/git-level',
+      path: '/',
       mode: 'history',
       name: 'quis',
       component: QuizView,
