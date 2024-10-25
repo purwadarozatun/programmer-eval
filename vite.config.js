805cf41645acae423e3a10f98e7eb2f124bba1a9
Purwa <purwadarozatunakis@gmail.com>
2024-10-25 11:14:26 +0700

    remove build

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: '/programmer-eval/',
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
